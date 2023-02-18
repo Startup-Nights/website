87c0d04f15fec4af420cbeac163841c0b2baa526
Mischa Jörg <mischa@ninetyfour.ch>
2023-02-18 20:39:40 +0100

    feat: update menu draft

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,73 +1,15 @@
-// import React from "react";
-// import Link from "next/link";
-// import { Container } from "../util/container";
-// import { SocialIcon } from "../items/social";
-//
-// export const Header = ({ data }) => {
-//     // If we're on an admin path, other links should also link to their admin paths
-//     const [prefix, setPrefix] = React.useState("");
-//
-//     React.useEffect(() => {
-//         if (window && window.location.pathname.startsWith("/admin")) {
-//             setPrefix("/admin");
-//         }
-//     }, []);
-//
-//     return (
-//
-//
-//         <div className='relative overflow-hidden bg-slate-900 text-slate-400'>
-//             <Container size="custom" className="py-0 relative z-10 max-w-8xl">
-//                 <div className="flex items-center justify-between gap-6">
-//                     <h4 className="select-none text-lg font-bold tracking-tight my-4 transition duration-150 ease-out transform">
-//                         <Link href="/" className="flex gap-1 items-center whitespace-nowrap tracking-[.002em]">
-//                             <img className="h-8 sm:h-12 w-auto" src="/logo/startup-nights.png" />
-//                         </Link>
-//                     </h4>
-//                     <div className="flex justify-end items-center">
-//                         <ul className="flex gap-6 sm:gap-8 lg:gap-10 tracking-[.002em] mx-4">
-//                             {data.nav && data.nav.map((item, i) => {
-//                                 return (
-//                                     <li key={`${item.label}-${i}`} className='hover:text-slate-100'>
-//                                         <Link
-//                                             href={`${prefix}/${item.href}`}
-//                                             className={`relative select-none text-base inline-block tracking-wide transition duration-150 ease-out py-8 px-4`}
-//                                         >
-//                                             {item.label}
-//                                         </Link>
-//                                     </li>
-//                                 );
-//                             })}
-//
-//                         </ul>
-//
-//                         <ul className="flex gap-6 sm:gap-4 lg:gap-5">
-//                             {data.social && data.social.map((item, i) => (
-//                                 <li key={`${item.title}-${i}`} >
-//                                     <a href={item.link} target='_blank' className="text-slate-400 hover:text-slate-100">
-//                                         <span className="sr-only">{item.title}</span>
-//                                         <SocialIcon name={item.title} className="h-6 w-6" aria-hidden="true" />
-//                                     </a>
-//                                 </li>
-//                             ))}
-//                         </ul>
-//
-//                     </div>
-//                 </div>
-//                 <div
-//                     className={`absolute h-1 bottom-0 left-4 right-4 -z-1 opacity-5`}
-//                 />
-//             </Container >
-//         </div >
-//     );
-// };
+import React, { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Bars3CenterLeftIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
+import { SocialIcon } from '../items/social'
 
-
-import React from 'react'
-import { Disclosure, } from '@headlessui/react'
-import { Bars3CenterLeftIcon, Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import Link from 'next/link';
-import { SocialIcon } from '../items/social';
+const navigation = [
+    { name: 'Product', href: '#' },
+    { name: 'Features', href: '#' },
+    { name: 'Marketplace', href: '#' },
+    { name: 'Company', href: '#' },
+]
 
 export const Header = ({ data }) => {
 
@@ -80,106 +22,112 @@ export const Header = ({ data }) => {
         }
     }, []);
 
-    return (
-        <Disclosure as="nav" className="bg-slate-900 text-slate-400">
-            {({ open }) => (
-                <>
-                    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                        <div className="flex h-16 justify-between">
-                            <div className="flex w-full justify-between">
-                                <div className="flex flex-shrink-0 items-center">
-                                    <Link href="/" className="flex gap-1 items-center whitespace-nowrap tracking-[.002em]">
-                                        <img className="h-8 sm:h-12 w-auto" src="/logo/startup-nights.png" />
-                                    </Link>
-                                </div>
-                                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
-                                    {data.nav && data.nav.map((item, i) => {
+    return (
+        <div className="bg-slate-900">
+            <div className="max-w-7xl mx-auto px-6 pt-6 lg:px-8">
+                <nav className="flex items-center justify-between" aria-label="Global">
+                    <div className="flex lg:flex-1">
+                        <Link href="/" className="-m-1.5 p-1.5 items-center whitespace-nowrap">
+                            <img className="h-8 sm:h-12 w-auto" src="/logo/startup-nights.png" />
+                        </Link>
+                    </div>
+                    <div className="flex lg:hidden">
+                        <button
+                            type="button"
+                            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
+                            onClick={() => setMobileMenuOpen(true)}
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3CenterLeftIcon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                    </div>
+                    <div className="hidden lg:flex lg:gap-x-12">
+                        {data.nav && data.nav.map((item, i: number) => {
+                            return (
+                                <Link
+                                    key={`${item.label}-${i}`}
+                                    href={`${prefix}/${item.href}`}
+                                    className='text-sm font-semibold leading-6 text-slate-400 hover:text-slate-100'
+                                >
+                                    {item.label}
+                                </Link>
+                            );
+                        })}
+                    </div>
+                    <div className="hidden lg:flex lg:flex-1 lg:justify-end space-x-2">
+                        {data.social && data.social.map((item, i: number) => (
+                            <a
+                                key={`${item.title}-${i}`}
+                                href={item.link} target='_blank'
+                                className='inline-flex items-center px-1 pt-1 text-sm font-medium text-slate-400 hover:text-slate-100'
+                            >
+                                <span className="sr-only">{item.title}</span>
+                                <SocialIcon name={item.title} className="h-6 w-6" aria-hidden="true" />
+                            </a>
+                        ))}
+                    </div>
+                </nav>
+                <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                    <Dialog.Panel className="fixed inset-0 z-10 overflow-y-auto backdrop-blur-md bg-slate-900/90 px-6 py-6 lg:hidden">
+                        <div className="flex items-center justify-between">
+                            <Link
+                                href="/"
+                                onClick={() => setMobileMenuOpen(false)}
+                                className="-m-1.5 p-1.5 flex gap-1 items-center whitespace-nowrap"
+                            >
+                                <img className="h-8 sm:h-12 w-auto" src="/logo/startup-nights.png" />
+                            </Link>
+                            <button
+                                type="button"
+                                className="-m-2.5 rounded-md p-2.5 text-gray-400"
+                                onClick={() => setMobileMenuOpen(false)}
+                            >
+                                <span className="sr-only">Close menu</span>
+                                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                        </div>
+                        <div className="mt-6 flow-root">
+                            <div className="-my-6 divide-y-2 divide-slate-600">
+                                <div className="space-y-2 py-6">
+                                    {data.nav && data.nav.map((item, i: number) => {
                                         return (
                                             <Link
                                                 key={`${item.label}-${i}`}
                                                 href={`${prefix}/${item.href}`}
-                                                className='inline-flex items-center px-1 pt-1 text-sm font-medium text-slate-400 hover:text-slate-100'
+                                                onClick={() => setMobileMenuOpen(false)}
+                                                className='-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-slate-400 hover:bg-gray-400/10'
                                             >
                                                 {item.label}
                                             </Link>
                                         );
                                     })}
-                                    {data.social && data.social.map((item, i) => (
-                                        <a
-                                            key={`${item.title}-${i}`}
-                                            href={item.link} target='_blank'
-                                            className='inline-flex items-center px-1 pt-1 text-sm font-medium text-slate-400 hover:text-slate-100'
-                                        >
-                                            <span className="sr-only">{item.title}</span>
-                                            <SocialIcon name={item.title} className="h-6 w-6" aria-hidden="true" />
-                                        </a>
-                                    ))}
+                                </div>
+                                <div className='py-6'>
+                                    <p className='-mx-3 block rounded-lg py-2 px-3 text-base leading-7 text-slate-400'>
+                                        <a href="https://startup-nights.ch" className="font-bold hover:text-gray-600">Startup Nights</a> - an event organized by <a href="https://ec-w.ch" className="font-bold hover:text-gray-600">ECW</a>
+
+                                    </p>
+                                    <div className='-mx-3 flex justify-start space-x-2'>
+                                        {data.social && data.social.map((item, i: number) => (
+                                            <a
+                                                key={`${item.title}-${i}`}
+                                                href={item.link} target='_blank'
+                                                onClick={() => setMobileMenuOpen(false)}
+                                                className='flex justify-center rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-slate-400 hover:bg-gray-400/10'
+                                            >
+                                                <span className="sr-only">{item.title}</span>
+                                                <SocialIcon name={item.title} className="h-6 w-6" aria-hidden="true" />
+                                            </a>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
-                            <div className="-mr-2 flex items-center sm:hidden">
-                                {/* Mobile menu button */}
-                                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-slate-800 hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                                    <span className="sr-only">Open main menu</span>
-                                    {open ? (
-                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                                    ) : (
-                                        <Bars3CenterLeftIcon className="block h-6 w-6" aria-hidden="true" />
-                                    )}
-                                </Disclosure.Button>
-                            </div>
                         </div>
-                    </div>
-
-                    <Disclosure.Panel className="sm:hidden bg-slate-800 rounded-xl mx-4 my-2 p-2">
-                        <div className="space-y-1 pt-2 pb-3">
-                            <Disclosure.Button
-                                as="a"
-                                href="#"
-                                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-slate-800 hover:text-slate-100"
-                            >
-                                Dashboard
-                            </Disclosure.Button>
-                            <Disclosure.Button
-                                as="a"
-                                href="#"
-                                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-slate-800 hover:text-slate-100"
-                            >
-                                Team
-                            </Disclosure.Button>
-                            <Disclosure.Button
-                                as="a"
-                                href="#"
-                                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-slate-800 hover:text-slate-100"
-                            >
-                                Projects
-                            </Disclosure.Button>
-                            <Disclosure.Button
-                                as="a"
-                                href="#"
-                                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-slate-800 hover:text-slate-100"
-                            >
-                                Calendar
-                            </Disclosure.Button>
-                        </div>
-                        <div className="">
-                            <div className={`m-2 grid grid-cols-${data.social.length}`}>
-                                {data.social && data.social.map((item, i) => (
-                                    <a
-                                        key={`${item.title}-${i}`}
-                                        href={item.link} target='_blank'
-                                        className='flex justify-center items-center py-2 rounded-xl text-sm font-medium text-slate-400 hover:bg-slate-700'
-                                    >
-                                        <span className="sr-only">{item.title}</span>
-                                        <SocialIcon name={item.title} className="h-6 w-6" aria-hidden="true" />
-                                    </a>
-                                ))}
-                            </div>
-                            {}
-                        </div>
-                    </Disclosure.Panel>
-                </>
-            )}
-        </Disclosure>
+                    </Dialog.Panel>
+                </Dialog>
+            </div>
+        </div>
     )
 }
