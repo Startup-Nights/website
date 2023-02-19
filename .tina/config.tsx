c8277a9dd04744ee8c3f7cb4f08a5393ee91ed53
Mischa Jörg <mischa@ninetyfour.ch>
2023-02-19 09:41:53 +0100

    feat: use digitalocean spaces as media provider

--- .tina/config.tsx
+++ .tina/config.tsx
@@ -13,17 +13,10 @@ const config = defineStaticConfig({
         process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
         process.env.HEAD!, // Netlify branch env
     token: process.env.TINA_TOKEN!,
-    media: {
-        // If you wanted cloudinary do this
-        // loadCustomStore: async () => {
-        //   const pack = await import("next-tinacms-cloudinary");
-        //   return pack.TinaCloudCloudinaryMediaStore;
-        // },
-        // this is the config for the tina cloud media store
-        tina: {
-            publicFolder: "public",
-            mediaRoot: "uploads",
-        },
+    loadCustomStore: async () => {
+        const pack = await import("next-tinacms-dos");
+        return pack.TinaCloudDOSMediaStore;
+
     },
     build: {
         publicFolder: "public", // The public asset folder for your framework
