cce7beea27d586183c4027cf49f3d45802817411
Mischa Jörg <mischa@ninetyfour.ch>
2023-04-21 19:02:36 +0200

    feat(buttons): add hover color

--- components/items/button.tsx
+++ components/items/button.tsx
@@ -2,7 +2,7 @@ export const Button = (props: any) => {
     return (
         <a href={props?.link}
             type="button"
-            className="rounded-full bg-sn-yellow px-6 py-3 text-sm font-bold text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="rounded-full bg-sn-yellow px-6 py-3 text-black hover:bg-sn-yellow-dark text-base font-semibold leading-7 sm:text-sm sm:leading-6 tracking-wide"
         >
             {props?.text}
         </a>
