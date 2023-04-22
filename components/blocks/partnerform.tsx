b1026bc1a1b1ec49544bedd02a3834e7ca36bf59
Mischa Jörg <mischa@ninetyfour.ch>
2023-04-22 20:36:02 +0200

    chore(partner): cleanup blocks and layout

--- components/blocks/partnerform.tsx
+++ components/blocks/partnerform.tsx
@@ -4,8 +4,8 @@ import { Section } from "../util/section"
 
 export default function PartnerForm({ data }) {
     return (
-        <Section>
-            <Container className={``} >
+        <div className="bg-sn-black-light">
+            <div className="max-w-7xl mx-auto p-24">
                 <div className='relative text-slate-200'>
                     <div>
                         <h3 className="text-base font-semibold leading-6 text-slate-200">Personal Information</h3>
@@ -25,7 +25,7 @@ export default function PartnerForm({ data }) {
                                         name="first-name"
                                         id="first-name"
                                         autoComplete="given-name"
-                                        className="w-full min-w-0 appearance-none rounded-md border-white/10 bg-gray-400/10 px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base leading-7 text-white placeholder-gray-500 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
+                                        className="w-full min-w-0 appearance-none rounded-xl border-white/10 bg-gray-400/10 px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base leading-7 text-white placeholder-gray-500 shadow-sm focus:border-sn-yellow focus:ring-sn-yellow sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
                                     />
                                 </div>
                             </div>
@@ -42,7 +42,7 @@ export default function PartnerForm({ data }) {
                                         placeholder="Muster"
                                         id="last-name"
                                         autoComplete="family-name"
-                                        className="w-full min-w-0 appearance-none rounded-md border-white/10 bg-gray-400/10 px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base leading-7 text-white placeholder-gray-500 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
+                                        className="w-full min-w-0 appearance-none rounded-xl border-white/10 bg-gray-400/10 px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base leading-7 text-white placeholder-gray-500 shadow-sm focus:border-sn-yellow focus:ring-sn-yellow sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
                                     />
                                 </div>
                             </div>
@@ -59,7 +59,7 @@ export default function PartnerForm({ data }) {
                                         placeholder="Muster AG"
                                         id="company"
                                         autoComplete="company"
-                                        className="w-full min-w-0 appearance-none rounded-md border-white/10 bg-gray-400/10 px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base leading-7 text-white placeholder-gray-500 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
+                                        className="w-full min-w-0 appearance-none rounded-xl border-white/10 bg-gray-400/10 px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base leading-7 text-white placeholder-gray-500 shadow-sm focus:border-sn-yellow focus:ring-sn-yellow sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
                                     />
                                 </div>
                             </div>
@@ -76,7 +76,7 @@ export default function PartnerForm({ data }) {
                                         placeholder="max@muster.ch"
                                         type="email"
                                         autoComplete="email"
-                                        className="w-full min-w-0 appearance-none rounded-md border-white/10 bg-gray-400/10 px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base leading-7 text-white placeholder-gray-500 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
+                                        className="w-full min-w-0 appearance-none rounded-xl border-white/10 bg-gray-400/10 px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base leading-7 text-white placeholder-gray-500 shadow-sm focus:border-sn-yellow focus:ring-sn-yellow sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
                                     />
                                 </div>
                             </div>
@@ -92,14 +92,15 @@ export default function PartnerForm({ data }) {
                                         placeholder="1000"
                                         name="budget"
                                         type="number"
-                                        className="w-full min-w-0 appearance-none rounded-md border-white/10 bg-gray-400/10 px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base leading-7 text-white placeholder-gray-500 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
+                                        className="w-full min-w-0 appearance-none rounded-xl border-white/10 bg-gray-400/10 px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base leading-7 text-white placeholder-gray-500 shadow-sm focus:border-sn-yellow focus:ring-sn-yellow sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
                                     />
                                 </div>
                             </div>
 
                             <div className="sm:col-span-6">
                                 <label htmlFor="about" className="block text-sm font-medium leading-6">
-                                    What kind of partnership do you have in mind?                            </label>
+                                    What kind of partnership do you have in mind?
+                                </label>
                                 <div className="mt-2">
                                     <textarea
                                         required={true}
@@ -107,7 +108,7 @@ export default function PartnerForm({ data }) {
                                         name="idea"
                                         placeholder="Are you interested in a booth, a workshop, ...?"
                                         rows={3}
-                                        className="w-full min-w-0 appearance-none rounded-md border-white/10 bg-gray-400/10 px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base leading-7 text-white placeholder-gray-500 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
+                                        className="w-full min-w-0 appearance-none rounded-xl border-white/10 bg-gray-400/10 px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base leading-7 text-white placeholder-gray-500 shadow-sm focus:border-sn-yellow focus:ring-sn-yellow sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
                                         defaultValue={''}
                                     />
                                 </div>
@@ -115,10 +116,10 @@ export default function PartnerForm({ data }) {
                                 </p>
                             </div>
 
-                            <div className="sm:col-span-2">
+                            <div className="">
                                 <button
                                     type="submit"
-                                    className="flex w-full items-center justify-center rounded-md bg-sky-500 py-1.5 px-3 text-base font-semibold leading-7 text-white hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400 sm:text-sm sm:leading-6"
+                                    className="flex w-full items-center justify-center rounded-xl bg-sn-yellow py-1.5 px-3 text-base font-semibold leading-7 text-black hover:bg-sn-yellow-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sn-yellow sm:text-sm sm:leading-6"
                                 >
                                     Send
                                 </button>
@@ -127,9 +128,8 @@ export default function PartnerForm({ data }) {
                         </div>
                     </form>
                 </div>
-            </Container>
-        </Section >
-
+            </div>
+        </div>
     )
 }
 
