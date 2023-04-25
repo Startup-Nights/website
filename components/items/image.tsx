860bc1080164c53d799f73e9648b43f9a6ee3562
Mischa Jörg <mischa@ninetyfour.ch>
2023-04-25 08:55:00 +0200

    fix(impressions): make image more responsive

--- components/items/image.tsx
+++ components/items/image.tsx
@@ -2,18 +2,15 @@ import Image from "next/image"
 
 export const RoundImage = ({ data }) => {
     return (
-        <div className="grid grid-cols-1 justify-items-center gap-y-8">
-            <div className="relative h-[400px] md:h-[500px] w-[400px] md:w-[500px]  rounded-full overflow-hidden">
-                <div className='absolute inset-0 w-full h-full'>
-                    <Image
-                        src={data.src}
-                        alt={data.alt}
-                        className="w-full h-full object-cover"
-                        width={500}
-                        height={500}
-                    />
-                </div>
-            </div>
+        <div className="relative ">
+            <Image
+                src={data.src}
+                alt={data.alt}
+                width="0"
+                height="0"
+                sizes="100vw"
+                className="w-full h-auto rounded-full"
+            />
         </div>
     )
 }
