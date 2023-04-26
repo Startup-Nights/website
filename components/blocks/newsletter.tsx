b00b90f5ebb7c9ffd2593d39d52b66881cb19396
Mischa Jörg <mischa@ninetyfour.ch>
2023-04-26 20:52:39 +0200

    feat(newsletter): toast notifications

--- components/blocks/newsletter.tsx
+++ components/blocks/newsletter.tsx
@@ -1,15 +1,17 @@
-import client from "@mailchimp/mailchimp_marketing";
-import { useRef, useState } from "react";
-
-client.setConfig({
-    apiKey: process.env.SLACK_WEBHOOK_URL as string,
-    server: "us16",
-});
+import { useRef, useState, Fragment } from "react";
+import { Transition } from '@headlessui/react'
+import { CheckCircleIcon } from '@heroicons/react/24/outline'
+import { ExclamationCircleIcon, XMarkIcon } from '@heroicons/react/20/solid'
 
 export default function Newsletter({ data }) {
     const [err, setErr] = useState(false);
     const [success, setSuccess] = useState(false);
     const [loading, setLoading] = useState(false);
+
+    const close = () => {
+        setSuccess(false)
+        setErr(false);
+    }
 
     const firstnameRef = useRef(null);
     const lastnameRef = useRef(null);
@@ -60,7 +62,7 @@ export default function Newsletter({ data }) {
         <div className="">
             <h3 className="text-sm font-semibold leading-6 text-white">Subscribe to our newsletter</h3>
             <p className="mt-2 text-sm leading-6 text-gray-300">
-                The latest news, articles, and resources, sent to your inbox weekly.
+                Sign up for the latest news, speaker announcements and discounts.
             </p>
             <form onSubmit={handleSubmit} className="mt-6 grid grid-cols-2 gap-y-3 gap-x-3">
 
@@ -132,18 +134,84 @@ export default function Newsletter({ data }) {
                         Subscribe
                     </button>
                 </div>
-
-                <div className="col-span-2">
-                    {success && (
-                        <p className="text-green-400 text-sm">Success! You are now subscribed to the newsletter 🥳</p>
-                    )}
-
-                    {err && (
-                        <p className="text-red-400 text-sm">Oh no! Something went wrong. Shoot us an email <a className="italic underline underline-offset-4" href="mailto:helo@startup-nights.ch">here</a> and we'll put you on the list.</p>
-                    )}
-                </div>
             </form>
-        </div>
 
+            <div
+                aria-live="assertive"
+                className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6 z-40"
+            >
+                <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
+                    <Transition
+                        show={err}
+                        as={Fragment}
+                        enter="transform ease-out duration-300 transition"
+                        enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+                        enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-sn-black ring-2 ring-sn-black-lightest shadow-xl shadow-sn-black">
+                            <div className="p-4">
+                                <div className="flex items-start">
+                                    <div className="flex-shrink-0">
+                                        <ExclamationCircleIcon className="h-6 w-6 text-red-400" aria-hidden="true" />
+                                    </div>
+                                    <div className="ml-3 w-0 flex-1 pt-0.5">
+                                        <p className="text-sm font-medium text-gray-200">Shoot! Something went wrong.</p>
+                                        <p className="mt-1 text-sm text-gray-400">Shoot us an email <a className="italic underline underline-offset-4" href="mailto:helo@startup-nights.ch">here</a> and we'll put you on the list.</p>
+                                    </div>
+                                    <div className="ml-4 flex flex-shrink-0">
+                                        <button
+                                            type="button"
+                                            className="inline-flex rounded-md bg-sn-black-lightest text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                            onClick={() => { close() }}
+                                        >
+                                            <span className="sr-only">Close</span>
+                                            <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Transition>
+
+                    <Transition
+                        show={success}
+                        as={Fragment}
+                        enter="transform ease-out duration-300 transition"
+                        enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+                        enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-sn-black ring-2 ring-sn-black-lightest shadow-xl shadow-sn-black">
+                            <div className="p-4">
+                                <div className="flex items-start">
+                                    <div className="flex-shrink-0">
+                                        <CheckCircleIcon className="h-6 w-6 text-green-400" aria-hidden="true" />
+                                    </div>
+                                    <div className="ml-3 w-0 flex-1 pt-0.5">
+                                        <p className="text-sm font-medium text-gray-200">Oh yes!</p>
+                                        <p className="mt-1 text-sm text-gray-400">You are now subscribed to the newsletter.</p>
+                                    </div>
+                                    <div className="ml-4 flex flex-shrink-0">
+                                        <button
+                                            type="button"
+                                            className="inline-flex rounded-md bg-sn-black-lightest text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                            onClick={() => { close() }}
+                                        >
+                                            <span className="sr-only">Close</span>
+                                            <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Transition>
+                </div>
+            </div>
+        </div>
     )
 }
--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -8,7 +8,7 @@ mailchimp.setConfig({
 // https://leerob.io/blog/mailchimp-next-js
 export default async (req, res) => {
     // visible on audience -> settings -> audience name and defaults
-    const list_id = "81a939ebc2";
+    const list_id = "dc844d3c35";
 
     try {
         const response = await mailchimp.lists.addListMember(
@@ -24,7 +24,7 @@ export default async (req, res) => {
             "status": "subscribed",
         });
 
-        if (response.status + ''  >= '400') {
+        if ((response.status as number) >= 400) {
             return res.status(400).json({
                 error: `There was an error subscribing to the newsletter. Shoot us an email at [hello@startup-nights.ch] and we'll add you to the list.`
             });
