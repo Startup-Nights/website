305eb28a80c418b451e4145a1226fbc7bf6026d0
Mischa Jörg <mischa@ninetyfour.ch>
2023-04-28 00:10:00 +0200

    feat(404): add text

---
+++ pages/404.tsx
@@ -0,0 +1,28 @@
+import { Hero } from "../components/blocks/hero";
+import { PartnerInfo } from "../components/blocks/partnerinfo";
+import { Layout } from "../components/layout";
+
+export default function FourOhFour() {
+  return (
+    <div className="bg-sn-black h-screen grid content-center">
+      <PartnerInfo
+        data={{
+          content_block: {
+            title: "Something's wrong here.",
+            content: [
+              "We couldn’t find the page you were looking for."
+            ],
+            cta: {
+              link: '/',
+              text: 'Go back to the landing page'
+            },
+            cta_secondary: {
+              link: '/contact',
+              text: 'Ping us'
+            }
+          }
+        }}
+      />
+</div>
+  );
+}
