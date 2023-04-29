b25abc1e047ab5f2ec3aa459bb52937a7832264d
Mischa Jörg <mischa@ninetyfour.ch>
2023-04-29 17:43:41 +0200

    feat(tito): add new page

---
+++ components/blocks/tito.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import type { Template } from "tinacms";
+import { ContentBlock, ContentBlockSchema } from "../items/contentblock";
+import Head from "next/head";
+
+export const Tito = ({ data }) => {
+    // https://stackoverflow.com/questions/55393226/disable-hydration-only-partially-hydrate-a-next-js-app
+
+    const [mounted, setMounted] = useState(false);
+  
+    useEffect(() => setMounted(true), []);
+    if (!mounted) return null;
+
+    return (
+        <div className="bg-sn-black">
+            <Head>
+                        <script src="https://js.tito.io/v2/with/inline" async></script>
+            </Head>
+            <div className="max-w-7xl mx-auto py-12 px-8 lg:p-24">
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-8 gap-x-24 items-center">
+                    <ContentBlock data={data?.content_block} />
+
+                    <div className="h-screen">
+                        <tito-widget
+                            event="ecw/startup-nights-2023"
+                            discount-code="SNCOMMUNITY"
+                            save-metadata-parameters="utm_*"
+                        ></tito-widget>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export const titoBlockSchema: Template = {
+    name: "tito",
+    label: "Tito",
+    fields: [
+        ContentBlockSchema,
+    ],
+};
