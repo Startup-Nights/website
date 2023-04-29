90d100b2171e48e0800961a74d48a3ef6ff10d3b
Mischa Jörg <mischa@ninetyfour.ch>
2023-04-29 22:00:37 +0200

    fix(fonts): add necessary weights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,17 @@ import "../styles.css";
 
 // https://nextjs.org/docs/basic-features/font-optimization
 const sourceSansPro = Source_Sans_Pro({
-  weight: '400',
+  weight: ['200', '300', '400', '600', '700', '900'],
+  style: ['normal', 'italic'],
   subsets: ['latin']
 })
 
 const App = ({ Component, pageProps }) => {
-  return <main className={sourceSansPro.className}>
-    <Component {...pageProps} />
-  </main>;
+  return (
+    <main className={sourceSansPro.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default App;
