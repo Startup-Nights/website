871ca5f5d321014adb72499e064c89f2b80a55fa
Mischa Jörg <mischa@ninetyfour.ch>
2023-04-29 21:51:44 +0200

    chore(fonts): switch to next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
+import { Source_Sans_Pro } from 'next/font/google'
 import "../styles.css";
 
+// https://nextjs.org/docs/basic-features/font-optimization
+const sourceSansPro = Source_Sans_Pro({
+  weight: '400',
+  subsets: ['latin']
+})
+
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return <main className={sourceSansPro.className}>
+    <Component {...pageProps} />
+  </main>;
 };
 
 export default App;
