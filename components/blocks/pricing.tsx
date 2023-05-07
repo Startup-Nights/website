433247d7ab79a0d90ecad018dcaf830fc7f24b72
Mischa Jörg <mischa@ninetyfour.ch>
2023-05-07 20:41:59 +0200

    fix(pricing): mobile layout

--- components/blocks/pricing.tsx
+++ components/blocks/pricing.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import { RadioGroup } from '@headlessui/react'
 import { CheckIcon } from '@heroicons/react/20/solid'
 import { Template } from 'tinacms'
 
@@ -65,66 +63,62 @@ function classNames(...classes) {
 }
 
 export const PricingTable = ({ data }) => {
-    const [frequency, setFrequency] = useState(frequencies[0])
-
     return (
         <div className="bg-sn-black">
             <div className="max-w-7xl mx-auto py-12 px-8 lg:p-24">
-                <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                    <div className="text-center mb-20">
-                        <h2 className="text-base font-medium leading-7 text-sn-yellow uppercase">
-                            {data.subtitle}
-                            Pricing
-                        </h2>
-                        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-200 sm:text-6xl">
-                            {data.title}
-                            Compare the different ticket categories.
-                        </h1>
-                    </div>
+                <div className="text-center mb-20">
+                    <h2 className="text-base font-medium leading-7 text-sn-yellow uppercase">
+                        {data.subtitle}
+                        Pricing
+                    </h2>
+                    <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-200 sm:text-6xl">
+                        {data.title}
+                        Compare the different ticket categories.
+                    </h1>
+                </div>
 
-                    <div className="isolate mx-auto mt-10 grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
-                        {tiers.map((tier) => (
-                            <div
-                                key={tier.name}
+                <div className="isolate mx-auto grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
+                    {tiers.map((tier) => (
+                        <div
+                            key={tier.name}
+                            className={classNames(
+                                'bg-sn-black-light',
+                                'rounded-3xl p-8'
+                            )}
+                        >
+                            <h3
+                                id={tier.name}
                                 className={classNames(
-                                    'bg-sn-black-light',
-                                    'rounded-3xl p-8'
+                                    'text-gray-200',
+                                    'text-lg font-semibold leading-8'
                                 )}
                             >
-                                <h3
-                                    id={tier.name}
-                                    className={classNames(
-                                        'text-gray-200',
-                                        'text-lg font-semibold leading-8'
-                                    )}
-                                >
-                                    {tier.name}
-                                </h3>
-                                <p className="mt-4 leading-6 text-gray-400">{tier.description}</p>
-                                <p className="mt-6 flex items-baseline gap-x-1">
-                                    <span className="text-4xl font-bold tracking-tight text-gray-200">{tier.price}</span>
-                                </p>
-                                <a
-                                    href={'#sn-tito-embedded-widget'}
-                                    aria-describedby={tier.name}
-                                    className={classNames(
-                                        'text-yellow ring-1 ring-inset ring-sn-yellow hover:ring-sn-yellow-dark transition-all',
-                                        'mt-6 block text-sn-yellow hover:text-black hover:bg-sn-yellow rounded-xl py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sn-yellow'
-                                    )}
-                                >
-                                    Get your ticket
-                                </a>
-                                <ul role="list" className="mt-8 space-y-3 leading-6 text-gray-200">
-                                    {tier.features.map((feature) => (
-                                        <li key={feature} className="flex gap-x-3">
-                                            <CheckIcon className="h-6 w-5 flex-none text-sn-yellow" aria-hidden="true" />
-                                            {feature}
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        ))}
-                    </div>
+                                {tier.name}
+                            </h3>
+                            <p className="mt-4 leading-6 text-gray-400">{tier.description}</p>
+                            <p className="mt-6 flex items-baseline gap-x-1">
+                                <span className="text-4xl font-bold tracking-tight text-gray-200">{tier.price}</span>
+                            </p>
+                            <a
+                                href={'#sn-tito-embedded-widget'}
+                                aria-describedby={tier.name}
+                                className={classNames(
+                                    'text-yellow ring-1 ring-inset ring-sn-yellow hover:ring-sn-yellow-dark transition-all',
+                                    'mt-6 block text-sn-yellow hover:text-black hover:bg-sn-yellow rounded-xl py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sn-yellow'
+                                )}
+                            >
+                                Get your ticket
+                            </a>
+                            <ul role="list" className="mt-8 space-y-3 leading-6 text-gray-200">
+                                {tier.features.map((feature) => (
+                                    <li key={feature} className="flex gap-x-3">
+                                        <CheckIcon className="h-6 w-5 flex-none text-sn-yellow" aria-hidden="true" />
+                                        {feature}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
