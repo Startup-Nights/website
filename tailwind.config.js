bce1f5de4116cf0c3294ec592e163717bdd3bca1
Mischa Jörg <mischa@ninetyfour.ch>
2023-05-11 21:35:54 +0200

    chore(config): cleanup tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,20 +11,6 @@ module.exports = {
                 'sn-yellow': '#FDC900',
                 'sn-yellow-dark': '#EBB400'
             },
-            fontSize: {
-                'h1': ['5.625rem', { lineHeight: '4.375rem' }],
-                'h2': ['4.375rem', { lineHeight: '3.375rem' }],
-                'h3': ['1.688rem', { lineHeight: '1.438rem' }],
-                'h4': ['1.438rem', { lineHeight: '1.438rem' }],
-                'h5': ['1.188rem', { lineHeight: '1.375rem' }],
-                'h6': ['0.75rem', { lineHeight: '0.938rem' }],
-                'p': ['1.125rem', { lineHeight: '1.5rem' }],
-                'psm': ['0.9rem', { lineHeight: '1.2rem' }],
-                'h1sm': ['2.625rem', { lineHeight: '2.375rem' }],
-                'h2sm': ['2.375rem', { lineHeight: '1.375rem' }],
-                'h3sm': ['1.388rem', { lineHeight: '1.238rem' }],
-                'h4sm': ['1.238rem', { lineHeight: '1.238rem' }],
-            },
         }
     },
     plugins: [
