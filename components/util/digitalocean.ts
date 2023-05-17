3e8d6593afb1f7f8f649e130ee9ed79e3db76404
Mischa Jörg <mischa@ninetyfour.ch>
2023-05-17 13:06:40 +0200

    chore(booth): file upload

--- components/util/digitalocean.ts
+++ components/util/digitalocean.ts
@@ -12,11 +12,11 @@ const s3Client = new S3({
 });
 
 // Uploads the specified file to the chosen path.
-export const uploadFile = async (name: string, data: any) => {
+export const uploadFile = async (name: string, data: any, filename: string) => {
     // Specifies a path within your bucket and the file to upload.
     const bucketParams = {
         Bucket: process.env.NEXT_PUBLIC_SPACES_NAME,
-        Key: name,
+        Key: '2023/booth/' + name + filename,
         Body: data,
     };
 
