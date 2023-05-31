b91d44192c7a9821fdb6c5e39cbe1808569ac4b7
Mischa Jörg <mischa@ninetyfour.ch>
2023-06-01 01:30:09 +0200

    chore(action): cleanup entrypoint

--- .github/actions/vercel-cypress/entrypoint.sh
+++ .github/actions/vercel-cypress/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh -l
 
-# https://vercel.com/docs/rest-api/endpoints#list-deployments
-# DEPLOYMENT_URL=$(curl --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v6/deployments" | jq -r '.deployments[0].url')
+# reference: https://vercel.com/docs/rest-api/endpoints#list-deployments
+echo "=> checking url from latest deployment for team ${2}"
 DEPLOYMENT_URL=$(curl -X GET "https://api.vercel.com/v6/deployments?teamId=$2" -H "Authorization: Bearer $1"  | jq -r '.deployments[0].url')
-echo "=> using deployment url: ${DEPLOYMENT_URL}"
+echo "=> found deployment url: ${DEPLOYMENT_URL}"
 
-# install the dependencies (important for typescript)
 yarn install
 
 export CYPRESS_BASE_URL="https://${DEPLOYMENT_URL}"
