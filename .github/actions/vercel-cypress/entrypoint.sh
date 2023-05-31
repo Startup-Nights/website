95ccaa175102d157b0280428b06de433da1a61f4
Mischa Jörg <mischa@ninetyfour.ch>
2023-06-01 00:04:54 +0200

    fix(actions): adjust curl script

--- .github/actions/vercel-cypress/entrypoint.sh
+++ .github/actions/vercel-cypress/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh -l
 
 # https://vercel.com/docs/rest-api/endpoints#list-deployments
-DEPLOYMENT_URL=$(curl --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v6/deployments" | jq -r '.deployments[0].url')
+# DEPLOYMENT_URL=$(curl --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v6/deployments" | jq -r '.deployments[0].url')
+DEPLOYMENT_URL=$(curl -X GET "https://api.vercel.com/v6/deployments" -H "Authorization: Bearer $1" | jq -r '.deployments[0].url')
 echo "=> using deployment url: ${DEPLOYMENT_URL}"
+
+# install the dependencies (important for typescript)
+npm install
 
 export CYPRESS_BASE_URL=${DEPLOYMENT_URL}
 npx cypress run --browser chrome --e2e
