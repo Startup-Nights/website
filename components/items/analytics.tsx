9afd23226e8aa24f9a7842ca622fdec798cdbfb2
Mischa Jörg <mischa@ninetyfour.ch>
2023-06-28 13:31:00 +0200

    fix(gtm): switch to another plugin

--- components/items/analytics.tsx
+++ components/items/analytics.tsx
@@ -1,38 +1,8 @@
-import Analytics from "analytics";
-import { Router } from "next/router";
-import googleAnalyticsPlugin from "@analytics/google-analytics";
-import googleTagManager from "@analytics/google-tag-manager";
+import TagManager from 'react-gtm-module';
 import { useEffect } from "react";
-
-const analytics = Analytics({
-  app: "startup-nights",
-  plugins: [
-    googleTagManager({
-      containerId: "GTM-5JJCJM9",
-    }),
-  ],
-});
 
 export const useAnalytics = () => {
   useEffect(() => {
-    // Fire initial page view
-    if (process.env.NODE_ENV === "production") {
-      analytics.page();
-    }
-    // Fire page views on routing
-    const handleRouteChange = (url) => {
-      if (process.env.NODE_ENV === "production") {
-        // We need to wrap it in a rAF to ensure the correct data is sent to Segment
-        // https://github.com/zeit/next.js/issues/6025
-        requestAnimationFrame(() => {
-          analytics.page();
-        });
-      }
-    };
-
-    Router.events.on("routeChangeComplete", handleRouteChange);
-    return () => Router.events.off("routeChangeComplete", handleRouteChange);
+    TagManager.initialize({ gtmId: 'GTM-5JJCJM9' });
   }, []);
-
-  return analytics;
 };
