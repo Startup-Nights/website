f8dea6a85cbbb0f03d07490b6d1e244805103d30
Mischa Jörg <mischa@ninetyfour.ch>
2023-08-11 20:47:57 +0200

    feat: update forms to use the latest do functions

--- pages/api/pitching.ts
+++ pages/api/pitching.ts
@@ -1,45 +1,54 @@
 import { IncomingWebhook } from '@slack/webhook';
-import { context, tracer } from '../../instrumentation';
 
 const url = process.env.SLACK_WEBHOOK_URL as string;
 const webhook = new IncomingWebhook(url);
 
-const stringify = (body: any) => {
-    return `
-Name: ${body["firstname"]} ${body["lastname"]}
-Email: ${body["email"]}
-Startup: ${body["startupname"]}
-Website: ${body["website"]}
-Pitchdeck: ${body["pitchdeck"]}
-Round: ${body["round"]}
-
-Problem: ${body["problem"]}
-Solution: ${body["solution"]}
-Approach: ${body["approach"]}
-User: ${body["user"]}
-
-Raising funds: ${body["funds"]}
-Pitching to others: ${body["pitching"]}
-
-Plan to make money: ${body["money"]}
-LinkedIn profiles: ${body["linkedin"]}
-`
-};
-
 export default async (req: any, res: any) => {
-    const span = tracer.startSpan('newsletter', undefined, context.active());
-    const body = req.body
-
     webhook.send({
-        text: `
-Pitching Signup ():
-${stringify(body)}
-`,
-    }).then(() => {
-        span.end()
-        return res.status(201).json({ error: '' });
+        text: `Data Backup - Pitching Signup
+${JSON.stringify(req.body)}`,
     }).catch(error => {
-        span.end()
         return res.status(500).json({ error: error.message || error.toString() });
     });
+
+    const response = await fetch('https://faas-fra1-afec6ce7.doserverless.co/api/v1/web/fn-70cb3437-eee1-474d-8ad6-387035b15671/website/sheets', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            id: '1ifvj7KmYvitjVGiKFceve7Zce9KlTijic2MDI_aXn9I',
+            range: 'A:Z',
+            data: toDataSlice(req.body),
+        }),
+    })
+
+    const { error } = await response.json()
+    if (error) {
+        return res.status(500).json({ error: error.message || error.toString() });
+    }
+
+    return res.status(201).json({ error: '' });
 }
+
+const toDataSlice = (body: any): string[] => {
+    const data: string[] = [
+        body.firstname,
+        body.lastname,
+        body.email,
+        body.startupname,
+        body.website,
+        body.pitchdeck,
+        body.round,
+        body.problem,
+        body.solution,
+        body.approach,
+        body.user,
+        body.funds,
+        body.pitching,
+        body.money,
+        body.linkedin,
+    ]
+    return data
+};
+
