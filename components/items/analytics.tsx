44824799de2166905915814d511736bd8b1f2714
Mischa Jörg <mischa@ninetyfour.ch>
2023-08-29 14:53:57 +0200

    fix(gtm): switch to html script tag

--- components/items/analytics.tsx
+++ components/items/analytics.tsx
@@ -10,7 +10,7 @@ export const useAnalytics = () => {
 
     return (
         <Head>
-            <Script src="https://cloud.ccm19.de/app.js?apiKey=89e260c85efd4f09942508c7065b0a08a35d71a51cab5e0b&amp;domain=64ede395cb4e98fd3603d3a3" referrerpolicy="origin" />
+            <script src="https://cloud.ccm19.de/app.js?apiKey=89e260c85efd4f09942508c7065b0a08a35d71a51cab5e0b&amp;domain=64ede395cb4e98fd3603d3a3" referrerpolicy="origin" />
         </Head>
     )
 };
