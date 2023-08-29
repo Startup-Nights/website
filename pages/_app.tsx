1423e97336caf0f245839e5cb6abb1ac3973ec50
Mischa Jörg <mischa@ninetyfour.ch>
2023-08-29 15:06:57 +0200

    fix(gtm): use consent in main app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import { Source_Sans_3 } from 'next/font/google'
 import "../styles.css";
-import { useAnalytics } from '../components/items/analytics';
 import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
+import Script from 'next/script';
 
 // https://nextjs.org/docs/basic-features/font-optimization
 const sourceSansPro = Source_Sans_3({
@@ -11,9 +14,13 @@ const sourceSansPro = Source_Sans_3({
 })
 
 const App = ({ Component, pageProps }) => {
-    useAnalytics()
+    useEffect(() => {
+        TagManager.initialize({ gtmId: 'GTM-5JJCJM9' });
+    }, []);
+
     return (
         <main className={sourceSansPro.className}>
+            <Script src="https://cloud.ccm19.de/app.js?apiKey=89e260c85efd4f09942508c7065b0a08a35d71a51cab5e0b&amp;domain=64ede395cb4e98fd3603d3a3" />
             <Analytics />
             <Component {...pageProps} />
         </main>
