3e55f4c086e1d7334a85b93b5fbd278cca099838
Mischa Jörg <mischa@ninetyfour.ch>
2023-08-30 17:46:29 +0200

    chore: remove link for jury members

--- components/items/members.tsx
+++ components/items/members.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 
 export const Members = ({ data }) => {
@@ -7,41 +6,27 @@ export const Members = ({ data }) => {
         <div className="">
             <ul role="list" className="grid gap-4 grid-cols-2 sm:gap-4 xl:grid-cols-3">
                 {data.map((person: any) => (
-                    <Link
-                        key={person?.name}
-                        href={person.linkedin ? person.linkedin : "/"}
-                        target="_blank"
-                        className="block group"
-                    >
-                        <li className="group relative transition-all">
-                            <div className="absolute invisible -top-3 -right-3 p-2 bg-white rounded-full text-black group-hover:visible">
-                                <ArrowTopRightOnSquareIcon
-                                    className="w-5 h-5"
-                                    strokeWidth={2}
+                    <li className="relative transition-all">
+                        <div className="grid grid-cols-1 text-center gap-x-6 p-4 bg-sn-black-lightest border-2 rounded-3xl border-transparent">
+                            <div className="flex justify-center mb-6">
+                                <Image
+                                    src={person.src ? person.src : "/user.svg"}
+                                    alt={person?.name ? person.name : "unknown"}
+                                    className="rounded-full bg-sn-black-light"
+                                    width={data?.diameter ? data.diameter : 100}
+                                    height={data?.diameter ? data.diameter : 100}
                                 />
                             </div>
-
-                            <div className="grid grid-cols-1 text-center gap-x-6 p-4 bg-sn-black-lightest border-2 rounded-3xl border-transparent group-hover:border-gray-200">
-                                <div className="flex justify-center mb-6">
-                                    <Image
-                                        src={person.src ? person.src : "/user.svg"}
-                                        alt={person?.name ? person.name : "unknown"}
-                                        className="rounded-full bg-sn-black-light"
-                                        width={data?.diameter ? data.diameter : 100}
-                                        height={data?.diameter ? data.diameter : 100}
-                                    />
-                                </div>
-                                <div>
-                                    <h3 className="text-sm font-semibold leading-7 tracking-tight">
-                                        {person?.name}
-                                    </h3>
-                                    <p className="text-sm font-semibold leading-6 text-gray-400">
-                                        {person?.position}
-                                    </p>
-                                </div>
+                            <div>
+                                <h3 className="text-sm font-semibold leading-7 tracking-tight">
+                                    {person?.name}
+                                </h3>
+                                <p className="text-sm font-semibold leading-6 text-gray-400">
+                                    {person?.position}
+                                </p>
                             </div>
-                        </li>
-                    </Link>
+                        </div>
+                    </li>
                 ))}
             </ul>
         </div>
