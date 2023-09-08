e69f10b81bbb8f4f652a1e0bd0aaf2031e6f03f5
Mischa Jörg <mischa@ninetyfour.ch>
2023-09-08 19:07:26 +0200

    chore: cleanup program component

--- components/blocks/program.tsx
+++ components/blocks/program.tsx
@@ -1,43 +1,7 @@
-import { useEffect, useState } from "react";
-import useSWR from 'swr';
 import type { Template } from "tinacms";
 import { ColorPickerInput } from "../fields/color";
-import Link from "next/link";
-import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
-
-type Event = {
-    time: string
-    location: string
-    name: string
-    speakers: string[]
-    format: string
-    link: string
-}
-
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const Program = ({ data }) => {
-    const [formats, setFormats] = useState([])
-    const [selectedFormat, setSelectedFormat] = useState('All');
-
-    const eventData = useSWR('/program_data.json', fetcher)
-
-    const events = eventData.data || []
-
-    useEffect(() => {
-        const tmp: string[] = ['All']
-        events.forEach((event: any) => {
-            if (tmp.indexOf(event.format) === -1) {
-                tmp.push(event.format)
-            }
-        })
-
-        setFormats(tmp)
-    }, [eventData.isLoading])
-
-    if (eventData.error) return <div>failed to load</div>
-    if (eventData.isLoading) return <div>loading...</div>
-
     return (
         <div className={data.background_color ? data.background_color : 'bg-sn-black'}>
             <div className="max-w-7xl mx-auto py-12 px-8 lg:p-24">
@@ -50,8 +14,7 @@ export const Program = ({ data }) => {
                     </h1>
                 </div>
 
-                <iframe className="w-full h-[1000px]" src="https://portal.startup-nights.ch/components/28350" />
-
+                <iframe className="w-full h-[800px] rounded-xl" src="https://portal.startup-nights.ch/components/28350" />
             </div>
         </div>
     );
@@ -81,113 +44,4 @@ export const programBlockSchema: Template = {
         },
     ],
 };
-
-const statuses = {
-    'Keynote': 'text-green-200 bg-green-800 ring-green-600/20',
-    'Panel Discussion': 'text-gray-200 bg-gray-800 ring-gray-500/10',
-    'Workshop': 'text-yellow-200 bg-yellow-800 ring-yellow-600/20',
-    'Pitching Session': 'text-emerald-200 bg-emerald-800 ring-emerald-600/20',
-    'Fireside Chat': 'text-pink-200 bg-pink-800 ring-pink-600/20',
-}
-
-function formatTextColor(department: string): string {
-    switch (department) {
-        case 'Keynote': {
-            return 'text-rose-400'
-        }
-
-        case 'Panel Discussion': {
-            return 'text-amber-400'
-        }
-
-        case 'Workshop': {
-            return 'text-violet-400'
-        }
-
-        case 'Pitching Session': {
-            return 'text-cyan-400'
-        }
-
-        case 'Fireside Chat': {
-            return 'text-emerald-400'
-        }
-
-        case 'Operations': {
-            return 'text-amber-400'
-        }
-    }
-
-    return 'text-sky-400'
-}
-
-function formatBgColor(department: string): string {
-    let color = 'bg-sky-600'
-
-    switch (department) {
-        case 'Keynote': {
-            color = 'bg-rose-600'
-            break
-        }
-
-        case 'Panel Discussion': {
-            color = 'bg-amber-600'
-            break
-        }
-
-        case 'Workshop': {
-            color = 'bg-violet-600'
-            break
-        }
-
-        case 'Pitching Session': {
-            color = 'bg-cyan-600'
-            break
-        }
-
-        case 'Fireside Chat': {
-            color = 'bg-emerald-600'
-            break
-        }
-    }
-
-    return color + ' text-black'
-}
-
-function formatBgHoverColor(department: string): string {
-    let color = 'hover:bg-sky-400';
-
-    switch (department) {
-        case 'Keynote': {
-            color = 'hover:bg-rose-400'
-            break
-        }
-
-        case 'Panel Discussion': {
-            color = 'hover:bg-amber-400'
-            break
-        }
-
-        case 'Workshop': {
-            color = 'hover:bg-violet-400'
-            break
-        }
-
-        case 'Pitching Session': {
-            color = 'hover:bg-cyan-400'
-            break
-        }
-
-        case 'Fireside Chat': {
-            color = 'hover:bg-emerald-400'
-            break
-        }
-
-        case 'Operations': {
-            color = 'hover:bg-amber-400'
-            break
-        }
-    }
-
-    return color + ' text-gray hover:text-black'
-}
 
