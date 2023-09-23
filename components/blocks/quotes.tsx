5db138a410c3fbfc513d151b368a47d88a5a766b
Mischa Jörg <mischa@ninetyfour.ch>
2023-09-23 10:51:50 +0200

    feat: update video preview block

--- components/blocks/quotes.tsx
+++ components/blocks/quotes.tsx
@@ -2,29 +2,34 @@ import React from "react";
 import type { Template } from "tinacms";
 import { ColorPickerInput } from "../fields/color";
 import Link from "next/link";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
 
 export const Quotes = ({ data }) => {
     return (
         <div className={data.background_color ? data.background_color : 'bg-sn-black'}>
-            <div className="py-12 px-8 lg:p-24">
+            <div className="pb-12 px-8 lg:pb-24">
                 <div className="max-w-7xl mx-auto text-center mb-12 sm:mb-20">
                     <h2 className="text-base font-medium leading-7 text-sn-yellow uppercase tracking-widest">
                         {data.subtitle}
                     </h2>
-                    <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-200 sm:text-6xl">
+                    <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-200">
                         {data.title}
                     </h1>
                 </div>
 
-                <div className="relative snap-x scroll-smoth flex gap-8 w-auto overflow-x-auto transition-all py-8">
+                <div className="relative snap-x scroll-smoth flex gap-8 w-auto overflow-x-scroll transition-all py-8">
                     {data?.videos && data.videos.map((item: any, i: number) => (
-                        <div className="group relative h-[200px] w-[240px] sm:h-[400px] sm:w-[480px] snap-center rounded-3xl overflow-hidden shrink-0">
+                        <div className="group relative h-[200px] w-[240px] snap-center rounded-3xl shrink-0  border-2 border-transparent hover:border-white">
+                            <div className="absolute -top-3 -right-3 p-2 bg-white rounded-full text-black z-10 invisible group-hover:visible">
+                                <ArrowTopRightOnSquareIcon className="w-5 h-5" strokeWidth={2} />
+                            </div>
+
                             <div className="absolute inset-0">
                                 <video
                                     autoPlay
                                     loop={true}
                                     muted
-                                    className="h-full w-full image object-cover"
+                                    className="h-full w-full image object-cover rounded-3xl"
                                     poster={item.video.fallback}
                                 >
                                     <source
@@ -33,14 +38,13 @@ export const Quotes = ({ data }) => {
                                     />
                                     Your browser does not support the video tag.
                                 </video>
-                                <div className="absolute inset-0 group-hover:mix-blend-multiply group-hover:bg-slate-400" />
                             </div>
 
                             <div className="absolute top-0 bottom-0 left-0 right-0">
                                 <Link
                                     href={item.video.src}
                                     target="_blank"
-                                    className="font-medium leading-7 text-gray-200 lg:text-2xl invisible group-hover:visible">
+                                    className="font-semibold leading-7 text-gray-200 text-xl invisible group-hover:visible">
 
                                     <div className="h-full flex justify-center items-center">
                                         <p>
