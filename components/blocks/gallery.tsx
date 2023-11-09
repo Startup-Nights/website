54e2fa2d0044ce28f844848f9844a117cb370334
Mischa Jörg <mischa@ninetyfour.ch>
2023-11-09 22:07:48 +0100

    gallery: add more photos

--- components/blocks/gallery.tsx
+++ components/blocks/gallery.tsx
@@ -2,10 +2,7 @@ import React, { useState } from "react";
 import type { Template } from "tinacms";
 import NextImage from "../items/nextimage";
 import { PhotoAlbum } from "react-photo-album";
-
 import "yet-another-react-lightbox/styles.css";
-
-// import optional lightbox plugins
 import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
 import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
@@ -29,7 +26,8 @@ export const Gallery = ({ data }) => {
                 </div>
 
                 <PhotoAlbum
-                    layout="rows"
+                    layout="masonry"
+                    columns={5}
                     photos={photos}
                     renderPhoto={NextImage}
                     defaultContainerWidth={1200}
@@ -80,14 +78,11 @@ const unsplashPhotos = [
     { id: "2023/dqh67ot5jhrya5zile2c", height: height, width: width },
     { id: "2023/gckexyj3sysawu2lan6a", height: height, width: width },
     { id: "2023/wxyznnrwo6ecocvjgwhx", height: height, width: width },
-
     { id: "2023/j7fwf7gcr0bqw4cokbzk", height: width, width: height },
     { id: "2023/o3hy4soge7mtgpqbuwfb", height: width, width: height },
     { id: "2023/ncodmyewevdsru6jjgwg", height: width, width: height },
     { id: "2023/wym0t4nsnalw0etqktwn", height: width, width: height },
-
     { id: "2023/l0sgoky9rse0yolupkqq", height: height, width: width },
-
     { id: "2023/vdkc5kshuwanp9iyjmtg", height: width, width: height },
     { id: "2023/ntbdiglqndhabqh7g3iy", height: width, width: height },
     { id: "2023/wrvtkxxfrfabhix1btv1", height: width, width: height },
@@ -119,6 +114,41 @@ const unsplashPhotos = [
     { id: "2023/je7z0mziixk5igvmteh1", height: width, width: height },
     { id: "2023/nmj0dxmgxsiwuwtwkwkq", height: width, width: height },
     { id: "2023/yfeyrqqdlhmwlkdkxdmy", height: width, width: height },
+    { id: "2023/g55fspanrbck4cyezhou", height: width, width: height },
+    { id: "2023/s1p3v0tkdukviviqzhnv", height: width, width: height },
+    { id: "2023/auvzmc4pafbq7xdowav1", height: width, width: height },
+    { id: "2023/ipblzl7i6rfavmjw2anl", height: width, width: height },
+    { id: "2023/i6y8dpwqad09vwqlnthf", height: width, width: height },
+    { id: "2023/jjp9desfktkp0roux8ep", height: width, width: height },
+    { id: "2023/bdbmif5zxhzneiv9oxrs", height: width, width: height },
+    { id: "2023/gktv78nzjrxg7htxhtjs", height: width, width: height },
+    { id: "2023/dcltyohru4pmx8lzv68b", height: width, width: height },
+    { id: "2023/v0z4yak11okxwy5ax7zo", height: width, width: height },
+    { id: "2023/unmhyjr9rzqihrdijxvt", height: width, width: height },
+    { id: "2023/lnxpoy4cqjam8i5hdcpk", height: width, width: height },
+    { id: "2023/hsqmt9tu3satqdb0juls", height: width, width: height },
+    { id: "2023/emefvimtlmdy64vaq39y", height: width, width: height },
+    { id: "2023/vssod7spkidrdmbexhvt", height: width, width: height },
+    { id: "2023/ojrqpayk4amphnteijmw", height: width, width: height },
+    { id: "2023/qzeo9au2daslobo6azkp", height: width, width: height },
+    { id: "2023/bjchdi1j9ks5pfjonkbr", height: width, width: height },
+    { id: "2023/muuy0xcpmz6qztfdvhaf", height: width, width: height },
+    { id: "2023/btal0mxi3iylbrjhud9i", height: width, width: height },
+    { id: "2023/eup8ov9iygq0mce7ipx0", height: width, width: height },
+    { id: "2023/gc8nsu0wacd3vhqpjorp", height: width, width: height },
+    { id: "2023/qjd8mmbnbbweo6da1j1e", height: width, width: height },
+    { id: "2023/nf9ci02poaavol2xwxat", height: width, width: height },
+    { id: "2023/aruvbscmqo77lemkpnrp", height: width, width: height },
+    { id: "2023/c5teyhqrjyo24zfhw04x", height: width, width: height },
+    { id: "2023/rokpd9jcnyhi0uriuffp", height: height, width: width },
+    { id: "2023/ll016zx9div5kztkn0p5", height: width, width: height },
+    { id: "2023/el0kwohp0wpj0td0bqoa", height: width, width: height },
+    { id: "2023/hzqirrr9c4kt7s24wkgb", height: width, width: height },
+    { id: "2023/liwehgnd9yscxs32zbr8", height: width, width: height },
+    { id: "2023/piyyirlwx70td0pfy0s2", height: width, width: height },
+    { id: "2023/hr3akpse6tfkxmugrktg", height: width, width: height },
+    { id: "2023/m5xhyvcteskuy9e6zedx", height: width, width: height },
+    { id: "2023/aziwcqnomcai7rsljrgt", height: width, width: height },
 ];
 
 const photos = unsplashPhotos.map((photo) => ({
@@ -135,7 +165,7 @@ const photos = unsplashPhotos.map((photo) => ({
     }),
 }));
 
-function normalizeSrc(src) {
+function normalizeSrc(src: string) {
     return src[0] === "/" ? src.slice(1) : src;
 }
 
