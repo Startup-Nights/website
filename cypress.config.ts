6f5544efa06e38aa9d63637343e19c733b2e8d09
Mischa Jörg <mischa@ninetyfour.ch>
2023-12-02 16:52:34 +0100

    cypress: add mochawesome reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    reporter: 'mochawesome',
+    reporterOptions: {
+        reportDir: 'cypress/results',
+        overwrite: false,
+        html: false,
+        json: true,
     },
-  },
+    e2e: {},
 });
