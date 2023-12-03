b34daeef8c090631b817f4367c56f9bf0121c184
Mischa Jörg <mischa@ninetyfour.ch>
2023-12-03 12:21:40 +0100

    cypress: add test for signups

---
+++ cypress/e2e/mailchimp.cy.ts
@@ -0,0 +1,25 @@
+describe('mailchimp signups', () => {
+    it('signs up for the newsletter', () => {
+        cy.visit('/')
+
+        cy.get('#newsletter-signup #firstname').type('Newsletter')
+        cy.get('#newsletter-signup #lastname').type('Test')
+        cy.get('#newsletter-signup #email').type('newslettertest@startup-nights.ch')
+        cy.get('#newsletter-signup button').click()
+
+        cy.contains('Oh yes!')
+        cy.contains('You are now subscribed to the newsletter.')
+    })
+
+    it('signs up for the preregistration', () => {
+        cy.visit('/tickets')
+
+        cy.get('#pre-register #firstname').type('Newsletter')
+        cy.get('#pre-register #lastname').type('Test')
+        cy.get('#pre-register #email').type('newslettertest@startup-nights.ch')
+        cy.get('#pre-register button').click()
+
+        cy.contains('Oh yes!')
+        cy.contains('You are now on the list for the pre-registration.')
+    })
+})
