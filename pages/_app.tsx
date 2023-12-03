56b507d087cf933a63f4781d0bf60002eb7d3af8
Mischa Jörg <mischa@ninetyfour.ch>
2023-12-03 12:05:54 +0100

    ccm: only show popup in production

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,43 +7,46 @@ import Script from 'next/script';
 
 // https://nextjs.org/docs/basic-features/font-optimization
 const sourceSansPro = Source_Sans_3({
-  weight: ['200', '300', '400', '600', '700', '900'],
-  style: ['normal', 'italic'],
-  subsets: ['latin']
+    weight: ['200', '300', '400', '600', '700', '900'],
+    style: ['normal', 'italic'],
+    subsets: ['latin']
 })
 
 // react strict mode, see
 // https://stackoverflow.com/a/74158550
 const App = ({ Component, pageProps }) => {
-  useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-5JJCJM9' });
-  }, []);
+    useEffect(() => {
+        TagManager.initialize({ gtmId: 'GTM-5JJCJM9' });
+    }, []);
 
-  // https://stackoverflow.com/a/62088643
-  // in case there is a hash in the route, scroll to it
-  useEffect(() => {
-    setTimeout(() => {
-      const hashId = window.location.hash
-      if (hashId) {
-        const element = document.querySelector(hashId)
-        if (element) {
-          element.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-            inline: 'nearest',
-          });
-        }
-      }
-    }, 500)
-  })
+    // https://stackoverflow.com/a/62088643
+    // in case there is a hash in the route, scroll to it
+    useEffect(() => {
+        setTimeout(() => {
+            const hashId = window.location.hash
+            if (hashId) {
+                const element = document.querySelector(hashId)
+                if (element) {
+                    element.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start',
+                        inline: 'nearest',
+                    });
+                }
+            }
+        }, 500)
+    })
 
-  return (
-    <main className={sourceSansPro.className}>
-      <Script src="https://cloud.ccm19.de/app.js?apiKey=89e260c85efd4f09942508c7065b0a08a35d71a51cab5e0b&amp;domain=64ede395cb4e98fd3603d3a3" />
-      <Analytics />
-      <Component {...pageProps} />
-    </main>
-  );
+    return (
+        <main className={sourceSansPro.className}>
+            {process.env.VERCEL_ENV === "production" && (
+                <Script src={`https://cloud.ccm19.de/app.js?apiKey=${process.env.CCM19_API_KEY}&amp;domain=${process.env.CCM19_DOMAIN}`} />
+            )}
+
+            <Analytics />
+            <Component {...pageProps} />
+        </main>
+    );
 };
 
 export default App;
