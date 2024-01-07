705627896d96e8e2841003ceb3556c6abd3fea8c
Mischa Jörg <mischa@ninetyfour.ch>
2024-01-07 14:53:07 +0100

    cypress: add script to cleanup audience members after tests

--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -7,16 +7,16 @@ mailchimp.setConfig({
 });
 
 // https://leerob.io/blog/mailchimp-next-js
-export default async (req, res) => {
+export default async (req: any, res: any) => {
     // visible on audience -> settings -> audience name and defaults
-    const list_id = "dc844d3c35";
+    const list_id = process.env.NEXT_MAILCHIMP_LIST;
 
     const email = (req.body.email as string).toLowerCase()
     const subscriber_hash = crypto.createHash('md5').update(email).digest('hex')
 
     try {
         // https://mailchimp.com/developer/marketing/api/list-members/add-or-update-list-member/
-        const response_update = await mailchimp.lists.setListMember(
+        await mailchimp.lists.setListMember(
             list_id,
             subscriber_hash,
             {
@@ -30,7 +30,7 @@ export default async (req, res) => {
             });
 
         // https://mailchimp.com/developer/marketing/api/list-member-tags/add-or-remove-member-tags/
-        const response_tags = await mailchimp.lists.updateListMemberTags(
+        await mailchimp.lists.updateListMemberTags(
             list_id,
             subscriber_hash,
             {
--- pages/api/preregister.ts
+++ pages/api/preregister.ts
@@ -2,47 +2,47 @@ import mailchimp from '@mailchimp/mailchimp_marketing';
 import crypto from 'crypto';
 
 mailchimp.setConfig({
-  apiKey: process.env.NEXT_MAILCHIMP as string,
-  server: "us16",
+    apiKey: process.env.NEXT_MAILCHIMP as string,
+    server: "us16",
 });
 
 // https://leerob.io/blog/mailchimp-next-js
-export default async (req, res) => {
-  // visible on audience -> settings -> audience name and defaults
-  const list_id = "dc844d3c35";
+export default async (req: any, res: any) => {
+    // visible on audience -> settings -> audience name and defaults
+    const list_id = process.env.NEXT_MAILCHIMP_LIST;
 
-  // https://dev.to/abdulrahman_sk/how-to-add-or-update-a-mailchimp-subscriber-using-rest-api-with-javascript-ill
-  const email = (req.body.email as string).toLowerCase()
-  const subscriber_hash = crypto.createHash('md5').update(email).digest('hex')
+    // https://dev.to/abdulrahman_sk/how-to-add-or-update-a-mailchimp-subscriber-using-rest-api-with-javascript-ill
+    const email = (req.body.email as string).toLowerCase()
+    const subscriber_hash = crypto.createHash('md5').update(email).digest('hex')
 
-  try {
-    // https://mailchimp.com/developer/marketing/api/list-members/add-or-update-list-member/
-    const response_update = await mailchimp.lists.setListMember(
-      list_id,
-      subscriber_hash,
-      {
-        "email_address": req.body.email,
-        "merge_fields": {
-          "FNAME": req.body.first,
-          "LNAME": req.body.last,
-        },
-        "status": "subscribed",
-        "status_if_new": "subscribed",
-      });
+    try {
+        // https://mailchimp.com/developer/marketing/api/list-members/add-or-update-list-member/
+        await mailchimp.lists.setListMember(
+            list_id,
+            subscriber_hash,
+            {
+                "email_address": req.body.email,
+                "merge_fields": {
+                    "FNAME": req.body.first,
+                    "LNAME": req.body.last,
+                },
+                "status": "subscribed",
+                "status_if_new": "subscribed",
+            });
 
-    // https://mailchimp.com/developer/marketing/api/list-member-tags/add-or-remove-member-tags/
-    const response_tags = await mailchimp.lists.updateListMemberTags(
-      list_id,
-      subscriber_hash,
-      {
-        tags: [
-          { name: "2024-pre-registration", status: "active" },
-        ]
-      }
-    )
+        // https://mailchimp.com/developer/marketing/api/list-member-tags/add-or-remove-member-tags/
+        await mailchimp.lists.updateListMemberTags(
+            list_id,
+            subscriber_hash,
+            {
+                tags: [
+                    { name: "2024-pre-registration", status: "active" },
+                ]
+            }
+        )
 
-    return res.status(201).json({ error: '' });
-  } catch (error) {
-    return res.status(500).json({ error: error.message || error.toString() });
-  }
+        return res.status(201).json({ error: '' });
+    } catch (error) {
+        return res.status(500).json({ error: error.message || error.toString() });
+    }
 }
