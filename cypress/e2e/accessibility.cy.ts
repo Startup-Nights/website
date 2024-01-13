882b5f1b5c697b16f58560be032e28b7694a85ca
Mischa Jörg <mischa@ninetyfour.ch>
2024-01-13 12:27:57 +0100

    cypress: add accessibility test

---
+++ cypress/e2e/accessibility.cy.ts
@@ -0,0 +1,32 @@
+describe('accessibility', () => {
+    const pages = [
+        '/',
+        '/about',
+        '/startup-city-winterthur',
+        '/hiring',
+        '/program',
+        '/crop',
+        '/speakers',
+        '/tickets',
+        '/terms-and-conditions',
+        '/privacy-policy',
+        '/imprint',
+        '/booth',
+        '/startups',
+        '/contact',
+        '/impressions',
+        '/partner',
+        '/partner-documentation',
+        '/partner-info',
+        '/pitching',
+        '/faq',
+    ]
+
+    pages.forEach(page => {
+        it(`'${page}' has no detectable a11y violations upon page load`, () => {
+            cy.visit(page)
+            cy.injectAxe()
+            cy.checkA11y()
+        })
+    })
+})
