934a1ffbeacbda014e4de25947dc5abf4dad96df
Mischa Jörg <mischa@ninetyfour.ch>
2024-01-21 15:22:09 +0100

    cypress: cleanup accessibility tests

---
+++ cypress/support/constants.ts
@@ -0,0 +1,28 @@
+export const viewports = [
+    'iphone-6',
+    'macbook-11',
+    'macbook-16'
+]
+
+export const pages = [
+    '/',
+    '/about',
+    '/startup-city-winterthur',
+    '/hiring',
+    '/program',
+    '/crop',
+    '/speakers',
+    '/tickets',
+    '/terms-and-conditions',
+    '/privacy-policy',
+    '/imprint',
+    '/booth',
+    '/startups',
+    '/contact',
+    '/impressions',
+    '/partner',
+    '/partner-documentation',
+    '/partner-info',
+    '/pitching',
+    '/faq',
+]
