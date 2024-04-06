c37c4c8b90627aecc5afdd8d87d9aa1aecbebdc8
Mischa Jörg <mischa@ninetyfour.ch>
2024-04-06 10:47:04 +0200

    button: open links in new tab if url is not relative
    For all links that start with 'http' or 'www', open the link in a new
    tab if not specified otherwise. Usually, this is for other websites and
    this way, (less?) no traffic is lost.
    
    Signed-off-by: Mischa Jörg <mischa@ninetyfour.ch>

--- components/items/button.tsx
+++ components/items/button.tsx
@@ -1,10 +1,22 @@
 import Link from "next/link"
 
+function infer_link_target(props: any): string {
+    const link = (props?.link ? props.link : '') as string
+
+    if (props?.new_tab) {
+        return props?.new_tab === true ? '_blank' : '_self'
+    }
+
+    return link.startsWith('http') || link.startsWith('www') ? '_blank' : '_self'
+}
+
 export const Button = (props: any) => {
+    const link = (props?.link ? props.link : '') as string
+
     return (
-        <Link href={props?.link ? props.link : ''}
+        <Link href={link}
             type="button"
-            target={props?.new_tab === true ? '_blank' : '_self'}
+            target={infer_link_target(props)}
             className="rounded-full transition-all bg-sn-yellow px-4 xl:px-6 py-1 xl:py-2 text-black border-2 border-sn-yellow hover:border-sn-yellow-dark hover:bg-sn-yellow-dark text-base font-semibold leading-7 sm:text-sm sm:leading-6 tracking-wide"
         >
             {props?.text}
@@ -13,10 +25,12 @@ export const Button = (props: any) => {
 }
 
 export const ButtonSecondary = (props: any) => {
+    const link = (props?.link ? props.link : '') as string
+
     return (
-        <Link href={props?.link ? props.link : ''}
+        <Link href={link}
             type="button"
-            target={props?.new_tab === true ? '_blank' : '_self'}
+            target={infer_link_target(props)}
             className={`rounded-full transition-all border-2 border-sn-yellow ${props?.without_background === true ? '' : 'bg-sn-black'} px-4 xl:px-6 py-1 xl:py-2 text-sn-yellow hover:bg-sn-yellow-dark hover:text-black hover:border-sn-yellow-dark text-base font-semibold leading-7 sm:text-sm sm:leading-6 tracking-wide`}
         >
             {props?.text}
