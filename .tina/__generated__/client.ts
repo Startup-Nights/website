f8f9782d670d03f5efa0ded8876a5a32d22783c5
Mischa Jörg <mischa@ninetyfour.ch>
2024-05-17 21:55:19 +0200

    tina: update generated files
    Signed-off-by: Mischa Jörg <mischa@ninetyfour.ch>

--- .tina/__generated__/client.ts
+++ .tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'd427ce3a726e278ef6c0a7d7c82fb3926fd3c6ac', queries,  });
 export default client;
   
