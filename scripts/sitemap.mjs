0ea9ddf148c68cb91b4fe29fec64dcd59332faef
Mischa Jörg <mischa@ninetyfour.ch>
2024-06-27 22:14:44 +0200

    fix: update script to wait for promise resolution
    Signed-off-by: Mischa Jörg <mischa@ninetyfour.ch>

--- scripts/sitemap.mjs
+++ scripts/sitemap.mjs
@@ -6,9 +6,9 @@ import prettier from 'prettier';
 const robots = `
 User-agent: *
 Disallow: /
-`
+`;
 
-const baseURL = 'https://www.startup-nights.ch'
+const baseURL = 'https://www.startup-nights.ch';
 
 // Update the robots.txt for preview deployments to make sure the preview 
 // deployments are not indexed.
@@ -19,46 +19,50 @@ async function updateRobotsTxt() {
     writeFileSync('public/robots.txt', robots);
   }
 }
- 
+
 // Generate the sitemap for the website and update it every time that the site 
 // is deployed.
 async function generate() {
-  const prettierConfig = await prettier.resolveConfig('./.prettierrc');
+  try {
+    const prettierConfig = await prettier.resolveConfig('./.prettierrc');
 
-  const pages = await globby([
-    'content/pages/*.md',
-    '!content/pages/terms-and-conditions.md',
-    '!content/pages/privacy-policy.md',
-  ]);
+    const pages = await globby([
+      'content/pages/*.md',
+      '!content/pages/terms-and-conditions.md',
+      '!content/pages/privacy-policy.md',
+    ]);
 
-  const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${pages
-          .map((page) => {
-            const path = page
-              .replace('content/pages', '')
-              .replace('.md', '');
-            const route = path === '/index' ? '' : path;
- 
-            return `
-              <url>
-                  <loc>${`${baseURL}${route}`}</loc>
-              </url>
-            `;
-          })
-          .join('')}
-    </urlset>
+    const sitemap = `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+          ${pages
+        .map((page) => {
+          const path = page
+            .replace('content/pages', '')
+            .replace('.md', '');
+          const route = path === '/index' ? '' : path;
+
+          return `
+                <url>
+                    <loc>${`${baseURL}${route}`}</loc>
+                </url>
+              `;
+        })
+        .join('')}
+      </urlset>
     `;
- 
-  const formatted = prettier.format(sitemap, {
-    ...prettierConfig,
-    parser: 'html',
-  });
- 
-  // eslint-disable-next-line no-sync
-  writeFileSync('public/sitemap.xml', formatted);
+
+    const formatted = await prettier.format(sitemap, {
+      ...prettierConfig,
+      parser: 'html',
+    });
+
+    // Ensure the data is a string before writing to file
+    writeFileSync('public/sitemap.xml', formatted);
+  } catch (error) {
+    console.error('Error generating sitemap:', error);
+  }
 }
- 
-generate();
-updateRobotsTxt();
+
+await generate();
+await updateRobotsTxt();
