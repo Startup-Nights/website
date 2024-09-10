2ad915e8ee789536666bdc8f7db8b652952bc13c
Mischa Jörg <mischa.joerg@bytesatwork.ch>
2024-09-10 13:25:05 +0200

    pages: newsletter: switch from mailchimp to hubspot
    Signed-off-by: Mischa Jörg <mischa.joerg@bytesatwork.ch>

--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -1,47 +1,26 @@
-import mailchimp from '@mailchimp/mailchimp_marketing';
-import crypto from 'crypto';
+import Hubspot from '@hubspot/api-client'
 
-mailchimp.setConfig({
-    apiKey: process.env.NEXT_MAILCHIMP as string,
-    server: "us16",
-});
-
-// https://leerob.io/blog/mailchimp-next-js
 export default async (req: any, res: any) => {
-    // visible on audience -> settings -> audience name and defaults
-    const list_id = process.env.NEXT_MAILCHIMP_LIST;
+    const hubspotClient = new Hubspot.Client({ accessToken: process.env.HUBSPOT_APP_TOKEN })
 
-    const email = (req.body.email as string).toLowerCase()
-    const subscriber_hash = crypto.createHash('md5').update(email).digest('hex')
+    const contact = {
+        properties: {
+            firstname: req.body.first,
+            lastname: req.body.last,
+            email: (req.body.email as string).toLowerCase()
+        },
+    }
 
     try {
-        // https://mailchimp.com/developer/marketing/api/list-members/add-or-update-list-member/
-        await mailchimp.lists.setListMember(
-            list_id,
-            subscriber_hash,
-            {
-                "email_address": req.body.email,
-                "merge_fields": {
-                    "FNAME": req.body.first,
-                    "LNAME": req.body.last,
-                },
-                "status": "subscribed",
-                "status_if_new": "subscribed",
-            });
-
-        // https://mailchimp.com/developer/marketing/api/list-member-tags/add-or-remove-member-tags/
-        await mailchimp.lists.updateListMemberTags(
-            list_id,
-            subscriber_hash,
-            {
-                tags: [
-                    { name: "Newsletter SN23", status: "active" },
-                ]
-            }
-        )
-
+        await hubspotClient.crm.contacts.basicApi.create(contact as any)
         return res.status(201).json({ error: '' });
     } catch (error) {
-        return res.status(500).json({ error: error.message || error.toString() });
+        if (error.code === 409) {
+            // this email address is already registered - skip it
+            // TODO: better error message?
+            return res.status(201).json({ error: '' });
+        }
+
+        return res.status(500).json({ error: error.message || error.toString(), });
     }
 }
