ae8cf6361d81c809cbd167896cc4474f390c30fe
Mischa Jörg <mischa@ninetyfour.ch>
2024-10-13 19:05:18 +0200

    scripts: ignor irrelevant data
    Signed-off-by: Mischa Jörg <mischa@ninetyfour.ch>

--- scripts/main.go
+++ scripts/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	data, err := os.ReadFile("/home/mischa/Downloads/registrations.csv")
+	data, err := os.ReadFile("/home/joergmis/Downloads/registrations.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,6 +27,24 @@ func main() {
 	}{}
 
 	output.Data = records
+
+	// needed: 0 1 5 6 8 35
+	required := func(i int) bool {
+		return i == 0 ||
+			i == 1 ||
+			i == 5 ||
+			i == 6 ||
+			i == 8 ||
+			i == 35
+	}
+
+	for i, row := range output.Data {
+		for j := range row {
+			if !required(j) {
+				output.Data[i][j] = ""
+			}
+		}
+	}
 
 	var out bytes.Buffer
 
